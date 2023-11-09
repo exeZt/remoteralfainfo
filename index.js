c94792e6ed80b254c4ae2cca22525d9c97ab8421
exezt- <106668197+exeZt@users.noreply.github.com>
2023-11-09 06:28:15 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const TelegramBot = require('node-telegram-bot-api'),
     }),
     os = require("os"),
     pty = require("node-pty"),
-    shell = os.platform() === 'win32' ? 'powershell.exe' : '/home/archives/IgorP/source/remoteProductInfo/:/usr/src/app';
+    shell = os.platform() === 'win32' ? 'powershell.exe' : 'bash';
 let term = pty.spawn(shell, [
     "ls\n"
 ], {});
